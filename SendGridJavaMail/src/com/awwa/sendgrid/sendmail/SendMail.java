fe9574127407e8e5a4429c42ddf0fd6f60a20b88
wataru <wataru@kke.co.jp>
2013-12-20 10:41:19 +0900

    First commit

---
+++ SendGridJavaMail/src/com/awwa/sendgrid/sendmail/SendMail.java
@@ -0,0 +1,10 @@
+package com.awwa.sendgrid.sendmail;
+
+public class SendMail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello java");
+	}
+
+}
